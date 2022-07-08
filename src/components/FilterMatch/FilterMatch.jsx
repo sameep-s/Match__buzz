9e53b9c325e3c5024bab43bb7361c1f2e741a309
sameep-s <sameep.sharma98@gmail.com>
2022-07-08 14:00:03 +0530

    teamYears

--- src/components/FilterMatch/FilterMatch.jsx
+++ src/components/FilterMatch/FilterMatch.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './filtersMatch.css';
 
+import { yearsLeague, teamNames } from '../../data';
+
 const FilterMatch = () => {
+
+    const [leagueYears, setLeagueYears] = useState([]);
+    const [teamNamesData, setTeamNamesData] = useState([]);
+
+
+    useEffect(() => {
+        setLeagueYears([...yearsLeague]);
+        setTeamNamesData([...teamNames]);
+
+        console.log(`teamNames:`, teamNames);
+        console.log(`team names:`, teamNamesData);
+    }, []);
+
     return (
         <>
             <div className="container__main__FilterMatch mt-2">
@@ -15,23 +30,20 @@ const FilterMatch = () => {
 
                 <div className="container__filters flex mt-1">
                     <div className="filter__Year">
-                        <label for='year' >Year: </label>
+                        <label htmlFor='year' >Year: </label>
                         <select name='year' id='years' >
-                            <option value='2017' >2017</option>
-                            <option value='2017' >2017</option>
-                            <option value='2017' >2017</option>
-                            <option value='2017' >2017</option>
+                            {[...leagueYears].reverse().map((year) => <option key={year} value={year} >{year}</option>)}
                         </select>
                     </div>
 
                     <div className="filter__team">
-                        <label for='team' >Team: </label>
+
+                        <label htmlFor='team' >Team: </label>
                         <select name='team' id='team' >
                             <option value='2017' >All</option>
-                            <option value='2017' >MI</option>
-                            <option value='2017' >2017</option>
-                            <option value='2017' >2017</option>
+                            {teamNamesData.map((data) => <option key={data.teamShortName} value={data.teamFullName} >{data.teamShortName}</option>)}
                         </select>
+
                     </div>
                 </div>
 
--- src/data/team-names.js
+++ src/data/team-names.js
@@ -1,1 +1,56 @@
-export const teamNames = [];
+export const teamNames = [
+    {
+        teamFullName: "Sunrisers Hyderabad",
+        teamShortName: "SRH",
+    },
+
+    {
+        teamFullName: "Royal Challengers Bangalore",
+        teamShortName: "RCB",
+    },
+
+    {
+        teamFullName: "Mumbai Indians",
+        teamShortName: "MI",
+    },
+
+    {
+        teamFullName: "Rising Pune Supergiant",
+        teamShortName: "RPS",
+    },
+
+    {
+        teamFullName: "Gujarat Lions",
+        teamShortName: "GL",
+    },
+
+    {
+        teamFullName: "Kolkata Knight Riders",
+        teamShortName: "KKR",
+    },
+
+    {
+        teamFullName: "Kings XI Punjab",
+        teamShortName: "KIXP",
+    },
+
+    {
+        teamFullName: "Delhi Daredevils",
+        teamShortName: "DD",
+    },
+
+    {
+        teamFullName: "Chennai Super Kings",
+        teamShortName: "CSK",
+    },
+
+    {
+        teamFullName: "Rajasthan Royals",
+        teamShortName: "RR",
+    },
+
+    {
+        teamFullName: "Deccan Chargers",
+        teamShortName: "DC",
+    }
+];
