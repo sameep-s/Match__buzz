5754fe7d8f213b2edb14e9412cb43016ed911da1
sameep-s <sameep.sharma98@gmail.com>
2022-07-08 11:23:43 +0530

    added: data to indexJs

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+import { yearsLeague, } from "./years__league";
+import { matchData } from "./match__data";
+import { teamNames } from "./team-names";
+
+export {
+    yearsLeague,
+    matchData,
+    teamNames
+}
